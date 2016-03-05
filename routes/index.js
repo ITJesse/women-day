6eb0d580e2ff7e34be9cbe25d9133dc27217a990
Jesse Zhu <jesse@itjesse.cn>
2016-03-06 02:59:15 +0800

    完成单页滚动

--- routes/index.js
+++ routes/index.js
@@ -2,23 +2,31 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('index', {title: '某个标题'});
-});
-
-router.get('/page2', function(req, res, next) {
-    res.render('page2', {title: '某个标题', count: 233333});
-});
-
-router.get('/page3', function(req, res, next) {
-    res.render('page3', {title: '某个标题'});
-});
-
-router.get('/page4', function(req, res, next) {
-    res.render('page4', {title: '某个标题', flowerId: 1, contentTitle: '战吼：', content: '我能吞下玻璃而不伤身体。'});
-});
-
-router.get('/page5', function(req, res, next) {
-    res.render('page5', {title: '某个标题', flowerId: 1, content: '我能吞下玻璃而不伤身体。我能吞下玻璃而不伤身体。我能吞下玻璃而不伤身体。我能吞下玻璃而不伤身体。我能吞下玻璃而不伤身体。'});
+    res.render('index', {
+        title: '您收到了一份节日祝福',
+        count: 233333,
+        list: [{
+            from: '张三',
+            to: '李四',
+            flower: '玫瑰花'
+        }, {
+            from: '张三',
+            to: '李四',
+            flower: '玫瑰花'
+        }, {
+            from: '张三',
+            to: '李四',
+            flower: '玫瑰花'
+        }, {
+            from: '张三',
+            to: '李四',
+            flower: '玫瑰花'
+        }, {
+            from: '张三',
+            to: '李四',
+            flower: '玫瑰花'
+        }]
+    });
 });
 
 module.exports = router;
